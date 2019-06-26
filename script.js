67a90c6d1cb00653c71f8ab936e4682bcfdb7145
Marsha Battee <marsha@sanesuite.com>
2019-06-25 18:02:18 -0600

    shopping-list part 2

--- script.js
+++ script.js
@@ -14,21 +14,43 @@ function handleItemForm (event, formRef) {
 		event.preventDefault();
 	}
 		addItemToShoppingList();
-
+		formRef.reset();
 		return false;
 }
 
 function	addItemToShoppingList (){
 	let itemName = document.getElementById("item-name");
 	let itemAmount = document.getElementById("item-amount");
+	let id = getRandomInt(0, 10000000);
+
+	let itemHtml = createListItemHtml(itemName.value, itemAmount.value, id);
 	let itemListRef = document.getElementById("shopping-list");
-	let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
 	itemListRef.insertAdjacentHTML ("afterend", itemHtml);
+
+	setDeleteButtonEvent(id);
 }
 
-function createListItemHtml(itemName, itemAmount) {
-	return `<li>
+function setDeleteButtonEvent(id){
+	let deleteButton = document.getElementById("button" + id);
+	deleteButton.addEventListener("click", () => {
+		removeListItem(id);
+	});
+}
+
+function createListItemHtml(itemName, itemAmount, id) {
+	return `<li id="item${id}">
 					${itemName} - ${itemAmount}
-				</li>		
-	`;
+					<button id="button${id}" type="button">Delete Item</button>
+				</li>`;
+}
+
+function removeListItem (id){
+	let listItem = document.getElementById("item" + id);
+	listItem.parentNode.removeChild(listItem);
+}
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
 }
